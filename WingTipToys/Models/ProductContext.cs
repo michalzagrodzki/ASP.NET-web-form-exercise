cb2d639dd99034748d4764922235a1e68a9ad459
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-08-03 14:51:35 +0200

    Review part of exercise.

--- WingTipToys/Models/ProductContext.cs
+++ WingTipToys/Models/ProductContext.cs
@@ -3,8 +3,7 @@ namespace WingTipToys.Models
 {
     public class ProductContext : DbContext
     {
-        public ProductContext()
-            : base("WingTipToys")
+        public ProductContext() : base("WingTipToys")
         {
         }
         public DbSet<Category> Categories { get; set; }
