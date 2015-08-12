cc98e82a4f1e90d0d244a651191703997f14949f
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-08-12 16:16:01 +0200

    Change routes to categories and products. Exercise 9.

--- WingTipToys/Global.asax.cs
+++ WingTipToys/Global.asax.cs
@@ -26,6 +26,25 @@ namespace WingTipToys
             // Create the custom role and user.
             RoleActions roleActions = new RoleActions();
             roleActions.AddUserAndRole();
+
+            // Add Routes.
+            RegisterCustomRoutes(RouteTable.Routes);
+
         }
+
+        void RegisterCustomRoutes(RouteCollection routes)
+        {
+            routes.MapPageRoute(
+                "ProductsByCategoryRoute",
+                "Category/{categoryName}",
+                "~/ProductList.aspx"
+            );
+            routes.MapPageRoute(
+                "ProductByNameRoute",
+                "Product/{productName}",
+                "~/ProductDetails.aspx"
+            );
+        }
+
     }
 }
--- WingTipToys/ProductList.aspx.cs
+++ WingTipToys/ProductList.aspx.cs
@@ -6,6 +6,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using WingTipToys.Models;
 using System.Web.ModelBinding;
+using System.Web.Routing;
 
 namespace WingTipToys
 {
@@ -16,13 +17,23 @@ namespace WingTipToys
 
         }
 
-        public IQueryable<Product> GetProducts([QueryString("id")] int? categoryId)
+        public IQueryable<Product> GetProducts(
+                    [QueryString("id")] int? categoryId,
+                    [RouteData] string categoryName)
         {
             var _db = new WingTipToys.Models.ProductContext();
             IQueryable<Product> query = _db.Products;
+
             if (categoryId.HasValue && categoryId > 0)
             {
                 query = query.Where(p => p.CategoryID == categoryId);
+            }
+
+            if (!String.IsNullOrEmpty(categoryName))
+            {
+                query = query.Where(p =>
+                                    String.Compare(p.Category.CategoryName,
+                                    categoryName) == 0);
             }
             return query;
         }
